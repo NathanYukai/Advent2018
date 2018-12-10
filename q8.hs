e6ed714a4f7ab5c676de6df7334101fbd5febba4
NathanYukai <zlcghoul@hotmail.com>
2018-12-10 14:59:32 +0000

    day8.txt

--- q8.hs
+++ q8.hs
@@ -1,5 +1,9 @@
+import qualified Text.Parsec     as P
+
 import qualified Common as C
 import Data.Char
+import Control.Monad
+import Data.Either
 import Data.List
 
 main = putStrLn ""
@@ -8,32 +12,46 @@ getInts :: String -> [Int]
 getInts strs = fmap read $ fmap snd $ filter (\(b,_) -> odd b) $ zip [1,2..] $ grouped
   where grouped = groupBy (\a b -> isDigit a == isDigit b) strs
 
--- return siblings, metaNum, and sum
-findMeta :: ([Int], Int, Int) -> Int -> ([Int], Int, Int)
-findMeta ([], n, ps) _ = ([],n, ps)
-findMeta (sibs, mn, prev_sum) 1 = ([], 0, su+prev_sum + (sum $ take mn sibs))
-  where  su = findMetaWithSibling $ drop mn sibs
-findMeta ((0:meta:rest), 0, prev_sum) num = findMeta (drop meta rest,
-                                                   0, prev_sum + (sum $ take meta rest)) (num-1)
-findMeta (stuff, mn, prev_sum) num =
-  case child of
-    0 -> findMeta (r,mn,prev_sum) num
-    _ -> findMeta (resultSIbs, retMn, resultNum) (num-1)
-  where  (resultSIbs, retMn, resultNum) = findMeta (rest, (meta), prev_sum + (sum $ take mn stuff)) child
-         r@(child:meta:rest) = drop (mn) stuff
-
-findMetaWithSibling :: [Int] -> (Int)
-findMetaWithSibling [] = 0
-findMetaWithSibling (0:numMeta:rest) = (sum (take numMeta $ reverse rest))
-findMetaWithSibling (numChild:numMeta:rest) = (sf + sum (take numMeta $ reverse rest))
-  where (sibs, mn, sf) = findMeta (children, 0, 0) numChild
-        children = reverse $ drop numMeta $ reverse rest
-
 process :: String -> IO String
 process fn = do
   strs <- C.getLines fn
   let str = head strs
       ints = getInts str
-      (res) = findMetaWithSibling ints
+      (res) = answer ints
+      res2 = answerTwo ints
   putStrLn $ show res
+  putStrLn $ show res2
   return ""
+
+type Parser = P.Parsec [Int] ()
+
+findMeta :: Parser Int
+findMeta = do
+  numCh <- P.anyToken
+  numMeta  <- P.anyToken
+  childs   <- sum <$> replicateM numCh findMeta
+  metas    <- sum <$> replicateM numMeta P.anyToken
+  pure $ childs + metas
+
+getAllChilds :: [Int] -> [Int] -> [Int]
+getAllChilds nums indexes = foldr (\a accu-> (nums !! (a-1)) : accu) [] allValid
+  where allValid = filter (\n -> (n -1) < length nums) indexes
+
+findMetaComplex :: Parser Int
+findMetaComplex = do
+  numCh <- P.anyToken
+  numMeta  <- P.anyToken
+  childs   <- replicateM numCh findMetaComplex
+  metas    <- replicateM numMeta P.anyToken
+  let
+      validChildren = getAllChilds childs metas
+      res = case childs of
+        [] -> sum metas
+        _ -> sum $ validChildren
+  pure res
+
+answerTwo :: [Int] -> Int
+answerTwo = fromRight 0 . P.parse findMetaComplex ""
+
+answer :: [Int] -> Int
+answer = fromRight 0 . P.parse findMeta ""
